b2908acff92d62a18f32a9e168db1e3217881221
Vitor Lardera <vitorlardera@gmail.com>
2026-03-18 18:18:34 -0300

    adioadicionando .env para variáveis globais

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,14 @@
 const request = require('supertest');
 const{ expect } = require('chai');
+require('dotenv').config()
+
+
+
 
 describe('Login', () => {
     describe('POST /login', () =>{
         it('Deve retornar 200 com token string quando usar credenciais válidas', async () => {
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 const{ expect } = require('chai');
+require('dotenv').config()
 
 describe('transferencias', () => {
     describe('POST /transferencias', () => {
         it('deve retornar sucesso com 201 quando o valor da transferencia for 10 ou acima', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                     .post('/login')
                     .set('Content-Type', 'application/json')
                     .send({
@@ -14,7 +15,7 @@ describe('transferencias', () => {
 
             const token = respostaLogin.body.token;
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
